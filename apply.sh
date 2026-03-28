06bcbb4534d79a7cdaae5978916cace362855936
alecripo <203631992+alecripo@users.noreply.github.com>
2026-03-28 23:30:03 +0200

    chore: add function to check if link already exists

--- apply.sh
+++ apply.sh
@@ -3,17 +3,23 @@ set -euo pipefail
 
 BASE_DIR="$HOME/tools/dotfiles"
 
+function check_link_exists() {
+    local link_name="$1"
+
+    if [[ -L "$link_name" ]]; then
+        echo "$link_name already synced; skipping..."
+        return 0
+    fi
+
+    return 1
+}
+
 function sync_bash() {
     echo "Applying bash configs..."
     local bashfiles_dir="$BASE_DIR/bash"
     for file in "$bashfiles_dir"/*; do
         local target_name="$HOME/.$(basename "$file")"
-        if [[ -f  "$target_name" || -L "$target_name" ]]; then
-            if ! diff -q "$target_name" "$bashfiles_dir/$file" > /dev/null; then
-                echo "$target_name already exists and differs; reconcile manually..."
-            else
-                echo "$target_name already synced; skipping..."
-            fi
+        if (check_link_exists "$target_name"); then
             continue
         fi
         echo -e "\t- creating symlink"
@@ -22,10 +28,35 @@ function sync_bash() {
     done
 }
 
+function sync_change_wallp_theme() {
+    local service_dir="$BASE_DIR/sync-theme-wallp/services"
+    local unit_storage_path="$HOME/.config/systemd/user"
+
+    if (! check_link_exists "$unit_storage_path/sync-theme-wallp.path"); then
+        ln -s "$service_dir/sync-theme-wallp.path" "$unit_storage_path/"
+    fi
+
+    if (! check_link_exists "$unit_storage_path/sync-theme-wallp.service"); then
+        ln -s "$service_dir/sync-theme-wallp.service" "$unit_storage_path/"
+    fi
+
+    if (! check_link_exists "$HOME/.local/share/services/envs/sync-theme-wallp.env"); then
+        ln -s "$service_dir/sync-theme-wallp.env" "$HOME/.local/share/services/envs/"
+    fi
+
+    if (! check_link_exists "$HOME/.local/bin/sync-theme-wallp"); then
+        ln -s "$service_dir/sync-theme-wallp.sh" "$HOME/.local/bin/sync-theme-wallp"
+    fi
+    chmod +x "$HOME/.local/bin/sync-theme-wallp"
+
+    systemctl --user daemon-reload
+    systemctl --user enable sync-theme-wallp.path
+    systemctl --user start sync-theme-wallp.path
+}
+
 function sync_cosmic() {
     echo "Syncing COSMIC configs..."
 
-    local services_dir="$BASE_DIR/services"
     local unit_storage_path="$HOME/.config/systemd/user"
     if [[ -v XDG_CONFIG_HOME ]]; then
         unit_storage_path="$XDG_CONFIG_HOME/systemd/user"
@@ -41,17 +72,9 @@ function sync_cosmic() {
         mkdir -p "$HOME/.local/share/services/envs"
     fi
 
+    sync_change_wallp_theme
 
-    ln -s "$services_dir/sync-theme-wallp/sync-theme-wallp.path" "$unit_storage_path/"
-    ln -s "$services_dir/sync-theme-wallp/sync-theme-wallp.service" "$unit_storage_path/"
-    ln -s "$services_dir/sync-theme-wallp/sync-theme-wallp.env" "$HOME/.local/share/services/envs/"
-    ln -s "$services_dir/sync-theme-wallp/sync-theme-wallp.sh" "$HOME/.local/bin/sync-theme-wallp"
-    chmod +x "$HOME/.local/bin/sync-theme-wallp"
-
-    systemctl --user daemon-reload
-    systemctl --user enable sync-theme-wallp.path
-    systemctl --user start sync-theme-wallp.path
 }
 
-sync_cosmic
 sync_bash
+sync_cosmic
